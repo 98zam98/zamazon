cd022672d4c95b61089d1d31cf85912bf2811118
zyad mackawy <zyadmackawy@gmail.com>
2022-11-29 12:25:47 +0200

    be4

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import data from './data';
 import seedRouter from './routes/seedRoutes';
 import productRouter from './routes/productRoutes';
 import userRouter from './routes/userRoutes';
 import orderRouter from './routes/orderRoutes';
+import path from 'path';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,9 +16,8 @@ dotenv.config();
 // connect to the DB "local"
 mongoose.connect((process.env as any).MONGODB_URI).then(()=>{
     console.log("DB");
+    console.log(`${(process.env as any).MONGODB_URI}`);
 }).catch(e=>console.log(e.message));
-
-
 
 // products\users will be created here
 app.use('/api/seed', seedRouter);
@@ -31,6 +30,12 @@ app.use('/api/users', userRouter);
 
 //  to place orders 
 app.use('/api/orders', orderRouter);
+
+// managing frontend
+// const __dirname = path.resolve();
+__dirname = path.resolve();
+app.use(express.static(path.join(__dirname,'/frontend/build')));
+app.get("8",(req, res)=>res.sendFile(path.join(__dirname,'/frontend/build/index.html')))
 
 // error handle
 
